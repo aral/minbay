ac8bca553f20b62723f87bd56abc47477576999f
Ev Bogue <ev@evbogue.com>
2016-12-18 14:27:17 -0600

    break core and basic modules into seperate repos (the ghetto way)

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 require('depject')(
-  // from more specialized to more general
-  require('./modules_extra'),
   require('./modules_basic'),
   require('./modules_core')
 ).app[0]()
---
+++ ipm.sh
@@ -0,0 +1,3 @@
+rm -rf modules_basic modules_core
+git clone ssb://%LBG/u6lKSeaTEd/8mqaJU4sF7ynn+g3YsGkngIwBUTw=.sha256 modules_basic
+git clone ssb://%QVRZ+aDC9lypSEJw/X234XxwkavZvKfdNgtBGpreP1U=.sha256 modules_core
