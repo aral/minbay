cd64d8e320be29c441b67993857d61f26f6d3aab
Ev Bogue <ev@evbogue.com>
2016-12-20 09:47:13 -0600

    spit out lite client invite on serve

--- serve.js
+++ serve.js
@@ -1,8 +1,19 @@
 var http = require('http');
 var serve = require('ecstatic');
+var client = require('ssb-client')
 
 http.createServer(
   serve({ root: __dirname + '/build/'})
 ).listen(3000);
 
-console.log('Serving the lite client at http://localhost:3000');
+opts = {"modern": true}
+
+client(function (err, sbot) {  
+  if(err) throw err
+  sbot.invite.create(opts, function (err, invite) {
+    if(err) throw err
+    var lite = invite
+    console.log('Your lite client is now listening at https://localhost:3000. Here\'s an invite\nhttps://localhost:3000#' + invite)
+  })
+})
+
