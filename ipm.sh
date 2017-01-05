5f5a65441e22d94dc8c7bfd5feb60eed164785cf
Ev Bogue <ev@evbogue.com>
2017-01-04 21:56:42 -0600

    add ssb-pkgs to ipm.sh

--- ipm.sh
+++ ipm.sh
@@ -1,5 +1,7 @@
 rm -rf modules_basic modules_core
-git clone http://git.scuttlebot.io/%25LBG%2Fu6lKSeaTEd%2F8mqaJU4sF7ynn%2Bg3YsGkngIwBUTw%3D.sha256 modules_basic
-git clone http://git.scuttlebot.io/%25QVRZ%2BaDC9lypSEJw%2FX234XxwkavZvKfdNgtBGpreP1U%3D.sha256 modules_core
-# git clone ssb://%LBG/u6lKSeaTEd/8mqaJU4sF7ynn+g3YsGkngIwBUTw=.sha256 modules_basic
-# git clone ssb://%QVRZ+aDC9lypSEJw/X234XxwkavZvKfdNgtBGpreP1U=.sha256 modules_core
+# git clone http://git.scuttlebot.io/%25LBG%2Fu6lKSeaTEd%2F8mqaJU4sF7ynn%2Bg3YsGkngIwBUTw%3D.sha256 modules_basic
+# git clone http://git.scuttlebot.io/%25QVRZ%2BaDC9lypSEJw%2FX234XxwkavZvKfdNgtBGpreP1U%3D.sha256 modules_core
+
+git clone ssb://%DcQl5bvPqEeU6ETOfdB98o6Q3rRXPuq4HbXaBt3tBSE=.sha256 ssb-pkgs
+git clone ssb://%LBG/u6lKSeaTEd/8mqaJU4sF7ynn+g3YsGkngIwBUTw=.sha256 modules_basic
+git clone ssb://%QVRZ+aDC9lypSEJw/X234XxwkavZvKfdNgtBGpreP1U=.sha256 modules_core
