cd9f8de213523cfab4a0b6dc9a3bdb1efc0f1035
Ev Bogue <ev@evbogue.com>
2017-05-26 14:35:39 -0400

    make sbot start script work, define port in one place

--- serve.js
+++ serve.js
@@ -1,12 +1,14 @@
-var http = require('http');
-var serve = require('ecstatic');
+var http = require('http')
+var serve = require('ecstatic')
 var client = require('ssb-client')
+
+const port = 3013  
 
 exports.serve = function() {
   http.createServer(
     serve({ root: __dirname + '/build/'})
-  ).listen(3013);
-  
+  ).listen(port)
+
   opts = {"modern": true}
   
   client(function (err, sbot) {  
@@ -14,7 +16,7 @@ exports.serve = function() {
     sbot.invite.create(opts, function (err, invite) {
       if(err) throw err
       var lite = invite
-      console.log('Your lite client is now listening at http://localhost:3013\nHere\'s an invite\nhttp://localhost:3013#' + invite)
+      console.log('Your lite client is now listening at http://localhost:' + port + '\nHere\'s an invite\nhttp://localhost:' + port + '#' + invite)
     })
   })
 }
