14a9cf95a84124805d09301b377633139888cfe7
Ev Bogue <ev@evbogue.com>
2017-12-14 14:50:13 -0600

    fix thread links not routing

--- modules/nav.js
+++ modules/nav.js
@@ -4,6 +4,10 @@ var HyperScroll = require('hyperscroll')
 var fs = require('fs')
 var path = require('path')
 
+//handle incase we run this inside `electro`
+if(!window.location.hash && isElectron()) //'undefined' !== typeof process && process.argv[2])
+  window.location.hash = process.argv[2]
+
 exports.gives = {
   nav: {
     screen: true, goto: true
@@ -48,15 +52,32 @@ exports.create = function (api) {
       container.replaceChild(el = HyperScroll(el), container.firstChild)
       el.dispatchEvent(new CustomEvent('focus', {target: page = el}))
     }
-    /*else {
+    else {
       container.replaceChild(index(str), container.firstChild)
-    }*/
+    }
   }
 
   return {
     nav: {
       screen: function () {
         var str = window.location.hash.substring(1)
+
+        /*window.onclick = function (ev) {
+          var href = ev.target.getAttribute('href')
+          if(href) {
+            ev.preventDefault()
+            goto(href)
+          }
+        }*/
+
+        if(isElectron()) {
+          window.onkeydown = function (ev) {
+            if(ev.altKey) {
+              if(ev.keyCode == 37) history.back()
+              else if(ev.keyCode == 39) history.forward()
+            }
+          }
+        }
 
         window.onhashchange = function () {
           load(location.hash.substring(1))
