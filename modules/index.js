6bfa6a64f8e5d82d436cbf446b6f7e27e035b28a
Ev Bogue <ev@evbogue.com>
2017-12-14 20:42:48 -0600

    combine modules and views

---
+++ modules/index.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  nav: require('./nav'),
+  sbot:  require('./sbot'),
+  message_name: require('./message-name.js'),
+  message_link: require('./message-link.js'),
+  threads: require('./threads'),
+  post: require('./post'),
+  compose: require('./compose'),
+  public: require('./public'),
+  private: require('./private'),
+  uxer: require('./uxer'),
+  message: require('./message'),
+  vote: require('./vote')
+}
+
